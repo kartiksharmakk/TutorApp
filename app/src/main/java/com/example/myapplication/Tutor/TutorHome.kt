c95ff7418320d3dd1b4cc1aa2fe475eeb6b2c73b
japneet <japneetsaini084@gmail.com>
2024-04-19 01:18:04 +0530

    tutor home and profile ui

--- app/src/main/java/com/example/myapplication/Tutor/TutorHome.kt
+++ app/src/main/java/com/example/myapplication/Tutor/TutorHome.kt
@@ -2,11 +2,42 @@ package com.example.myapplication.Tutor
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
 import com.example.myapplication.R
+import com.example.myapplication.databinding.ActivityTutorHomeBinding
 
 class TutorHome : AppCompatActivity() {
+    lateinit var binding: ActivityTutorHomeBinding
+    lateinit var navController: NavController
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_tutor_home)
+        binding = ActivityTutorHomeBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        navController = findNavController(R.id.nav_tutor_host_fragment)
+        binding.tutorBottomNav.setOnNavigationItemSelectedListener {
+            when(it.itemId){
+                R.id.st_home -> {
+                    navController.navigate(R.id.tutorHomeFragment)
+                    true
+                }
+                R.id.createGroup -> {
+                    navController.navigate(R.id.tutorCreateTestFragment)
+                    true
+                }
+                R.id.tutor_profile->{
+                    navController.navigate(R.id.tutor_profile)
+                    true
+                }
+                else->{
+                false
+                }
+            }
+        }
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp() || super.onSupportNavigateUp()
     }
 }
