6bd6c522b295719d6cbe245c16dad8b77a1a4a45
japneet <japneetsaini084@gmail.com>
2024-04-24 00:28:39 +0530

    show all students

--- app/src/main/java/com/example/myapplication/Tutor/CreateGroupFragment.kt
+++ app/src/main/java/com/example/myapplication/Tutor/CreateGroupFragment.kt
@@ -16,6 +16,7 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import androidx.core.content.ContextCompat
+import androidx.navigation.fragment.findNavController
 import com.bumptech.glide.Glide
 import com.bumptech.glide.request.RequestOptions
 import com.example.myapplication.Data.DataModel
@@ -55,6 +56,9 @@ class CreateGroupFragment : Fragment() {
         binding.imgCreateGroupEditDP.setOnClickListener {
             checkPermission(ImageType.DISPLAY)
             currentImageType = ImageType.DISPLAY
+        }
+        binding.imgCreateGroupAddMembers.setOnClickListener {
+            findNavController().navigate(R.id.addStudents)
         }
         binding.btnCreateGroup.setOnClickListener {
             createGroup()
